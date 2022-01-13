d3413bf6f652e591ee74ab1e824f2c6b39a2f849
Sarah Melanie White <sawrrawr@gmail.com>
2022-01-12 21:27:47 -0330

    added function to compare objects for a match, including if some of the values of an object are arrays

---
+++ eqObjects.js
@@ -0,0 +1,65 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❤️ ❤️ ❤️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  //make sure arrays are the same length
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+  // compares the indexes of each array, one by one
+    for (let i = 0; i < array1.length; i++) {
+      // if arrays do not match, return false
+      if (array1[i] !== array2[i]) {
+       return false;
+      }
+    }
+ }
+ return true;
+};
+
+// returns 'true' if both objects have identical keys and identical values
+// otherwise return 'false'
+
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+  for (key of object1Keys) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      };
+    }
+    else if (object1[key]!== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
+assertEqual(eqObjects(cd, cd2), false);
+
+//TEST CODE
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// eqObjects(ab, ba); // => true
+
+// const abc = { a: "1", b: "2", c: "3" };
+// eqObjects(ab, abc); // => false
+
+// assertEqual(eqObjects(ab, ba), true);
+// assertEqual(eqObjects(ab, abc), false);
